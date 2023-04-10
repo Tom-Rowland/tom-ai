5c4df5c859283847049144cb6d3ea5df4752d332
Tom <tomzarowland@gmail.com>
2023-04-10 18:57:00 +0100

    Update workflow to use github actions secrets

--- credentials.py
+++
@@ -1,6 +0,0 @@
-credentials_dict = {
-    'openai_token':'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',
-    'resembleai':{
-        'projectid':'46ebea48',
-        'voiceid':'a2b46b42',
-        'token': 'pQeNBrlR38ksVL7iP3SKdwtt'}}
