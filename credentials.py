65d25943f26c71ba6c68c541a2ac9153eefdf773
Tom <tomzarowland@gmail.com>
2023-04-10 17:48:22 +0100

    Set up credentials (API keys, resemble.ai project/voice IDs)

---
+++ credentials.py
@@ -0,0 +1,6 @@
+credentials_dict = {
+    'openai_token':'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',
+    'resembleai':{
+        'projectid':'46ebea48',
+        'voiceid':'a2b46b42',
+        'token': 'OUtnKtRxRDlcYySZQNp5Zgtt'}}
