645b2bb701688081daef620d2df849ebaf1b6382
Tom <tomzarowland@gmail.com>
2023-04-11 17:13:40 +0100

    Add test for checking secrets are ingested correctly

--- config.py
+++ config.py
@@ -3,11 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 try: # This should run locally
-    from credentials import credentials_dict
-    OPENAI_TOKEN = credentials_dict['openai_token']
-    RESEMBLEAI_TOKEN = credentials_dict['resembleai']['token']
-    RESEMBLEAI_PROJECTID = credentials_dict['resembleai']['projectid']
-    RESEMBLEAI_VOICEID = credentials_dict['resembleai']['voiceid']
+    from credentials import OPENAI_TOKEN, RESEMBLEAI_TOKEN, RESEMBLEAI_PROJECTID, RESEMBLEAI_VOICEID
+
 except: # This should run on GitHub Actions
     OPENAI_TOKEN = os.environ['OPENAI_TOKEN']
     RESEMBLEAI_TOKEN = os.environ['RESEMBLEAI_TOKEN']
