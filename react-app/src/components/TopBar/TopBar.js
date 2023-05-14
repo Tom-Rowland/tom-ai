c88d2d28b432d3ed3a7c90e2985e049043e70d06
Tom <tomzarowland@gmail.com>
2023-05-14 09:07:37 +0100

    Top bar

---
+++ react-app/src/components/TopBar/TopBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faFileAlt } from '@fortawesome/free-brands-svg-icons';
+import './TopBar.css'
+
+const TopBar = () => {
+    return (
+        <div className="top-bar">
+            <div className="app-name">tom.ai</div>
+            <div className="buttons">
+                <a href="https://github.com/Tom-Rowland/tom-ai" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon ={faGithub} />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;
