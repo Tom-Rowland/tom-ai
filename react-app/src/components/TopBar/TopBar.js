21fb5fbff4906702ecd79e279f6bccc9c88c6edd
Tom <tomzarowland@gmail.com>
2023-05-14 09:09:25 +0100

    Remove unused import

--- react-app/src/components/TopBar/TopBar.js
+++ react-app/src/components/TopBar/TopBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faFileAlt } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './TopBar.css'
 
 const TopBar = () => {
